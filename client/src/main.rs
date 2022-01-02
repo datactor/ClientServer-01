3229cc25f0cda2b94de63f39d25a4bfb3d8fe2b8
datactor <datactress@gmail.com>
2022-01-03 00:23:23 +0900

    Add: Echo server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,31 +1,37 @@
 use std::net::{TcpStream};
 use std::io::{Read, Write};
 use std::str::from_utf8;
+use std::str;
 
 fn main() {
     match TcpStream::connect("localhost:3333") {
         Ok(mut stream) => {
             println!("Successfully connected to server in port 3333");
+            loop {
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).unwrap();
+                let msg = input.as_bytes();
 
-            let msg = b"Hello!";
+                stream.write(msg).unwrap();
 
-            stream.write(msg.as_ref()).unwrap();
-            println!("Sent Hello, awaiting reply...");
-
-            let mut data = [0 as u8; 6]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == msg {
-                        println!("Reply is ok!");
-                    } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text);
+                let mut data = [0 as u8; 256]; // using 256 byte buffer
+                match stream.read(&mut data) {
+                    Ok(_) => {
+                        let text = str::from_utf8(&data[0..input.len()]).unwrap();
+                        if text.eq(&input) {
+                            println!("Receiving Message : {}" , from_utf8(&data[0..input.len()-1]).unwrap());
+                        } else {
+                            let text = from_utf8(&data).unwrap();
+                            println!("Unexpected reply: {}", text);
+                        }
+                    },
+                    Err(e) => {
+                        println!("Failed to receive data: {}", e);
+                        break;
                     }
-                },
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
                 }
             }
+
         },
         Err(e) => {
             println!("Failed to connect: {}", e);
